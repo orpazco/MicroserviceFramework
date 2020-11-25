0e988a40dc4f295f785056b122624ab870a0ec10
tomergaf <tomergafsou@gmail.com>
2020-11-25 18:22:58 +0200

    fixed  Ewok tests

--- src/test/java/bgu/spl/mics/EwokTest.java
+++ src/test/java/bgu/spl/mics/EwokTest.java
@@ -19,49 +19,52 @@ public class EwokTest {
 
     @Test
     public void testSerial(){
-        // check if serialNumber Valid??
-     }
-
+        // check if serialNumber is not null
+        assertNotNull(initialSN);
+    }
+     @Test
+     public void testInitialAvailability(){
+        // check if initialized as available
+        assertTrue(ewok.isAvailable(), "Expected: " + true + "Actual:" + ewok.isAvailable());
+    }
+    @Test
     public void testAcquireAvailable(){
-        // check if can acquire
-        assertTrue(ewok.isAvailable());
         // check if boolean changed after acquiring
         ewok.acquire();
         assertFalse(ewok.isAvailable());
         // check if serial is the same
-        assertEquals(initialSN, ewok.getSerialNumber(), "Expected: "+ initialSN + "Actual: " + ewok.getSerialNumber());
+        assertEquals(initialSN, ewok.getSerialNumber(), "Expected SN: "+ initialSN + "Actual: " + ewok.getSerialNumber());
     }
-
+    @Test
     public void testAcquireUnavailable(){
         // get availability - should get false
+        ewok.acquire();
         boolean availability = ewok.isAvailable();
-        assertFalse(availability, "Availability not as expected");
         // attempt to acquire and check if boolean changed  should not change
         ewok.acquire();
         assertEquals(availability, ewok.isAvailable(),"Expected: " + availability + "Actual: " + ewok.isAvailable());
         // check if serial is the same
-        assertEquals(initialSN, ewok.getSerialNumber(), "Expected: "+ initialSN + "Actual: " + ewok.getSerialNumber());
+        assertEquals(initialSN, ewok.getSerialNumber(), "Expected SN: "+ initialSN + "Actual: " + ewok.getSerialNumber());
     }
-
+    @Test
     public void testReleaseUnavailable(){
-        // check if can be released
-        assertFalse(ewok.isAvailable());
-        // check if boolean changed
+        // set Ewok as unavailable
+        ewok.acquire();
+        // check if boolean changed after releasing
         ewok.release();
         assertTrue(ewok.isAvailable());
         // check if serial number changed
-        assertEquals(initialSN, ewok.getSerialNumber(), "Expected: "+ initialSN + "Actual: " + ewok.getSerialNumber());
+        assertEquals(initialSN, ewok.getSerialNumber(), "Expected SN: "+ initialSN + "Actual: " + ewok.getSerialNumber());
     }
-
+    @Test
     public void testReleaseAvailable(){
         // get availability - should get true
         boolean availability = ewok.isAvailable();
-        assertTrue(availability, "Availability not as expected");
         // check if boolean changed - should not change
         ewok.release();
         assertEquals(availability, ewok.isAvailable(),"Expected: " + availability + "Actual: " + ewok.isAvailable());
         // check if serial changed
-        assertEquals(initialSN, ewok.getSerialNumber(), "Expected: "+ initialSN + "Actual: " + ewok.getSerialNumber());
+        assertEquals(initialSN, ewok.getSerialNumber(), "Expected SN: "+ initialSN + "Actual: " + ewok.getSerialNumber());
     }
 
 }
