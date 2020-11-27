0cd7b68f6cea28c1e331a898332496e5be706b76
tomergaf <tomergafsou@gmail.com>
2020-11-27 16:07:38 +0200

    improved message bus broadcast tests and tidy up

--- src/test/java/bgu/spl/mics/MessageBusBroadcastTest.java
+++ src/test/java/bgu/spl/mics/MessageBusBroadcastTest.java
@@ -1,7 +1,6 @@
 package bgu.spl.mics;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import  bgu.spl.mics.application.messages.*;
 import  bgu.spl.mics.application.services.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,92 +14,115 @@ public class MessageBusBroadcastTest {
     TestMic2 bm2;
     MessageBus messageBus;
 
+    @BeforeAll
+    public void setUp(){
+        messageBus = MessageBusImpl.getInstance();
+    }
+
     @BeforeEach
-    public void Setup(){
+    public void preTest(){
         be1 = new TestBCastEvent1();
         be2 = new TestBCastEvent2();
         bm1 = new TestMic1();
         bm2 = new TestMic2();
-        bm3 = new TestMic1(); //sender
-        messageBus = new MessageBusImpl();
         messageBus.register(bm1);
         messageBus.register(bm2);
-        messageBus.register(bm3);
     }
 
     @Test
     public void testBSingleton(){
-    // test singleton
+        // test singleton
+        MessageBus messageBusOther = MessageBusImpl.getInstance(); // assign a message bus to a different reference
+        assertSame(messageBus, messageBusOther, "Not the same instance"); // check if both are the same instance
     }
-
+    @Test
     public void testBCastSendAndReceive() {
         // tests if subscribers receive the right messages
-        messageBus.subscribeBroadcast(be1.getClass(), bm1);
-        messageBus.sendBroadcast(be1);
+        messageBus.subscribeBroadcast(be1.getClass(), bm1); // bm1 subscribes to be1
+        messageBus.sendBroadcast(be1); // a be1 broadcast is sent to all subscribers
         Message b = null;
         try {
+            //attempt to retrieve a message from bm1's queue
             b = messageBus.awaitMessage(bm1);
         }
-        catch (IllegalStateException | InterruptedException e){
-            // do something
+        catch (IllegalStateException | InterruptedException e){ // if bm1 is unregistered or interrupted exception is thrown
+            fail("Exception thrown: " + e + "\nPossibly because of unregistered micro-service or test interruption");
         }
-        assertEquals(b ,be1 );
+        assertEquals(b ,be1, "Expected message type: " + be1.getClass() + "Actual: " + b.getClass());
     }
+    @Test
     public void testBCastMultiSubAndNoneSub(){
-        // tests if multisubscribers (more than one subscription) get correct ones, and that non-subscribers are excluded
-        messageBus.subscribeBroadcast(be1.getClass(), bm1);
-        messageBus.subscribeBroadcast(be1.getClass(), bm2);
-        messageBus.subscribeBroadcast(be2.getClass(), bm2);
+        // tests if multi-subscribers (more than one subscription) get correct ones, and that non-subscribers are excluded
+        messageBus.subscribeBroadcast(be1.getClass(), bm1); // bm1 subscribes to be1
+        messageBus.subscribeBroadcast(be1.getClass(), bm2); // bm2 subscribes to be1
+        messageBus.subscribeBroadcast(be2.getClass(), bm2); // bm2 subscribes to be2 - now bm1 is subscribed to be1 and bm2 to be1 and be2
         messageBus.sendBroadcast(be2);
         messageBus.sendBroadcast(be1);
         Message a = null;
         Message b = null;
+        // in the first inspection, we check that both messages pulled from the queues of bm1, and bm2 are different events
         try {
             a = messageBus.awaitMessage(bm1);
             b = messageBus.awaitMessage(bm2);
         }
         catch (IllegalStateException | InterruptedException e){
-            // do something
+            fail("Exception thrown: " + e + "\nPossibly because of unregistered micro-service or test interruption");
         }
-        assertNotEquals(a ,b ); //bm1 should have be1 only in its queue and bm2 should have be2,be1
+        // expecting to compare a message of type be1 and be2
+        assertNotEquals(a ,b);
+        // in the second inspection bm1 should have be1 only in its queue and bm2 should have be2,be1
         try {
+            // pull the second message from bm2's queue - a be2 type message
             b = messageBus.awaitMessage(bm2);
         }
         catch (IllegalStateException | InterruptedException e){
-            // do something
+            fail("Exception thrown: " + e + "\nPossibly because of unregistered micro-service or test interruption");
         }
-        assertEquals(a ,b ); // now bm2 should have only be1 in its queue as well
+        // now bm2 should have only be1 in its queue as well
+        assertEquals(a ,b);
 
 
     }
-
-    public void testBCastUnregisteredAweait() throws InterruptedException {
+    @Test
+    public void testBCastUnregisteredAwait() {
         //tests that an unregistered waiter throws an exception
-        messageBus.unregister(bm1);
+        messageBus.unregister(bm1); // bm1 was registered ot the message bus and now is removed
         try{
             messageBus.awaitMessage(bm1);
         }
         catch (IllegalStateException e) {
-            assert true;
+            // test passes if the correct exception is thrown
+            assertTrue(true);
         }
-        assert false;
-    }
+        catch (Exception e){
+        fail("Expecting IllegalStateException and got: " + e);
 
-    public void testBcastNoSubscribers(){
+        }
+        fail("Excpected IllegalStateException and got no exception");
+    }
+    @Test
+    public void testBCastNoSubscribers(){
         // tests that a message that was sent while no one was subscribing to it is deleted (no one receives it after subscribing)
-        messageBus.sendBroadcast(be1);
-        messageBus.subscribeBroadcast(be1.getClass(),bm1);
-        messageBus.subscribeBroadcast(be2.getClass(),bm1);
-        messageBus.sendBroadcast(be2);
+        messageBus.sendBroadcast(be1); // send a be1 type broadcast while no one is subscribed to it
+        messageBus.subscribeBroadcast(be1.getClass(),bm1); // bm1 subscribes to be1
+        messageBus.subscribeBroadcast(be2.getClass(),bm1); // bm1 subscribes to be2
+        messageBus.sendBroadcast(be2); // a be2 is broadcasted
         Message a = null;
+        // inspects bm1's queue for messages - expecting it only to contain the be2 message
         try{
             a = messageBus.awaitMessage(bm1);
         }
         catch (IllegalStateException | InterruptedException e){
-            assert false;
+            fail("Exception thrown: " + e);
         }
-        assertEquals(a, be2); // checks if the message received is of type 1 or 2 - should get 2 , that means first message was not saved
+        // checks if the message received is the first one sent or the second, if got the second one - that means first message was not saved
+        assertEquals(a, be2, "Expected message type: " + be1.getClass() + "Actual: " + a.getClass());
 
+    }
+    @AfterEach
+    public void tearDown(){
+        messageBus.unregister(bm1);
+        messageBus.unregister(bm2);
     }
 
 
