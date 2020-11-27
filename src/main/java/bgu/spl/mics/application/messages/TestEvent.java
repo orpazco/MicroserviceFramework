66f48fa37addf1482e7a52b9ec44a7673757fb93
Orpazc <orpaz129@gmail.com>
2020-11-27 14:43:28 +0200

    add test event for mbi tests

---
+++ src/main/java/bgu/spl/mics/application/messages/TestEvent.java
@@ -0,0 +1,9 @@
+package bgu.spl.mics.application.messages;
+
+import bgu.spl.mics.Event;
+
+/***
+ * TestEvent is used for the {@link bgu.spl.mics.MessageBusImpl} unit tests
+ */
+public class TestEvent implements Event<String> {
+}
