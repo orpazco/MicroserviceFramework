dc5fa2c61523a5e5b6066608e151ac32dec03066
Orpazc <orpaz129@gmail.com>
2020-12-06 15:32:38 +0200

    add json serialize & deserialize

---
+++ src/main/java/bgu/spl/mics/application/passiveObjects/JsonHandler.java
@@ -0,0 +1,40 @@
+package bgu.spl.mics.application.passiveObjects;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JsonHandler {
+    /**
+     * deserialize a json file to an object
+     * @param path of the json file
+     * @return FlowData object with all the information from the json
+     * @throws IOException in case of wrong path
+     */
+    public static FlowData deserialize(String path) throws IOException {
+        Gson gson = new Gson();
+        Reader reader = Files.newBufferedReader(Paths.get(path));
+        // convert json file to flowData object
+        FlowData flowData = gson.fromJson(reader, FlowData.class);
+        reader.close();
+        return flowData;
+    }
+
+    /**
+     * serialize an object to a json file to a given path
+     * @param diary the object that contains all data to serialize
+     * @param path of the created json file
+     * @throws IOException in case of wrong path
+     */
+    public static void serialize(Diary diary, String path) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        // convert diary object to json format
+        FileWriter writer = new FileWriter(path);
+        gson.toJson(diary, writer);
+        writer.close();
+    }
+}
